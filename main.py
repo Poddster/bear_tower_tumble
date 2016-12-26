b203da5165bd665005de31ccf3e4116a890f2bf3
Lee Griffiths <poddster@gmail.com>
2016-12-26 23:28:29 +0000

    Add vauge outline.

--- main.py
+++ main.py
@@ -8,6 +8,64 @@ from pygame.locals import *
 _logger = logging.getLogger(__name__)
 
 
+def AxisAlignedBoundingBox():
+    pass
+
+AABB = AxisAlignedBoundingBox
+
+# A physicsy-thing that can topple over
+def Toppleable():
+    # A position in ???-space? 0-1? Should just keep this data in Bear for now?
+    # infact it probably should be calculated depend on bears current squish?
+    centre_of_gravity = None
+
+    # "If an object is tilted it will topple over if a vertical line from its
+    # centre of gravity falls outside its base."
+
+
+def Bear():
+    # Bears are a really simple 2-level AABB hierarchies.
+    # The AABBs of the BearParts are the actual hit-targets.
+    aabb = None
+    bear_parts = []
+    name = "TI Bear"
+
+
+def BearPart():
+    # aabb is in Bear-space.
+    aabb = None
+    mass = None
+
+    # density = mass / volume
+    # with density and gravity we can work out downwards pressure
+    # on bears lower in the stack? we'd need each part to have a 'robustness'
+    # to see how resistant to pressure it is. aka compressability?
+    # maybe the designer can just say what material each bear-part is made of,
+    # and the game engine works out these values. e.g. TI-Bear is mostly
+    # marshmellow. Pushface is ancient stuffing. Prison-Bear has a cast iron
+    # ball and regular stuffing.
+    # If the pressue is too great the part squishes down into the block below,
+    # somehow? This could cause the COG to redistribute and the bear could now
+    # topple?
+    # Bears can't be compressed past a single block?
+    @property
+    def density():
+        pass
+
+    @property
+    def pressure():
+        # Pressure = Force / Area. Force = mass * g
+        pass
+
+    compressability = None
+    current_compression = None
+
+    name = "left arm"
+
+    texture = None
+
+
+
 def main(args):
     _logger.info("main")
 
