8110f306da43e5cee4fd792adb85e0e3ec37aa88
Lee Griffiths <poddster@gmail.com>
2016-12-30 17:49:56 +0000

    Use pygame's Sprite and Group framework
    Should mean we avoid having to manually implement dirty flags etc.
    
    Also Tidy up some of the sketched out classes.
    
    Resolves #1

--- main.py
+++ main.py
@@ -8,15 +8,8 @@ from pygame.locals import *
 _logger = logging.getLogger(__name__)
 
 
-def AxisAlignedBoundingBox():
-    pass
-
-
-AABB = AxisAlignedBoundingBox
-
-
 # A physicsy-thing that can topple over
-def Toppleable():
+class Toppleable():
     # A position in ???-space? 0-1? Should just keep this data in Bear for now?
     # infact it probably should be calculated depend on bears current squish?
     centre_of_mass = None
@@ -25,19 +18,12 @@ def Toppleable():
     # centre of gravity falls outside its base."
 
 
-def Material():
+class Material():
     # probably just need mass (for 1m**3).
     mass = None
 
-def Bear():
-    # Bears are a really simple 2-level AABB hierarchies.
-    # The AABBs of the BearParts are the actual hit-targets.
-    aabb = None
-    bear_parts = []
-    name = "TI Bear"
 
-
-def BearPart():
+class BearPart():
     # aabb is in Bear-space.
     aabb = None
     material = None
@@ -45,15 +31,40 @@ def BearPart():
     texture = None
 
 
+class Bear(pygame.sprite.Sprite):
+    # Bears are a really simple 2-level geometry hierarchies.
+    # The geom of the BearParts are the actual hit-targets.
+    # aabb = None
+    # geom = None
+    # bear_parts = []
+    # name = "TI Bear"
+
+    def __init__(self, texture_path="assets/crap_ti_bear.png"):
+        super().__init__()
+        self.image = pygame.image.load(texture_path).convert_alpha()
+        self.rect = self.image.get_rect()
+
+    def update(self, *args):
+        None
+
+
 def main(args):
     _logger.info("main")
 
     pygame.init()
     screen_surface = pygame.display.set_mode((640, 480))
+    pygame.display.set_caption('Bear Tower Tumble')
 
-    bear_sprite = pygame.image.load("assets/crap_ti_bear.png").convert_alpha()
+    def clear_background(surf, rect):
+        surf.fill((100, 149, 237), rect)
 
-    pygame.display.set_caption('Hello World')
+    clear_background(screen_surface, screen_surface.get_rect())
+
+    bear_sprite = Bear()
+
+    all_sprites = pygame.sprite.Group([bear_sprite])
+    all_sprites.clear(screen_surface, clear_background)
+
     while True:
         for event in pygame.event.get():
             # print(event)
@@ -62,9 +73,9 @@ def main(args):
                 pygame.quit()
                 return
 
-        screen_surface.fill((100, 149, 237))
-        sprite_pos = (0, 0)
-        screen_surface.blit(bear_sprite, sprite_pos)
+        all_sprites.update()
+        all_sprites.draw(screen_surface)
+        # FIXME Not sure if I should use flip() or update() with groups
         pygame.display.flip()
 
 
